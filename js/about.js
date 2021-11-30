b0e8bf4d2774b799062321b3b5b071f2a7957cf0
Daniel Contero <daniel.contero@gmail.com>
2021-11-30 23:13:49 +0000

    added About form and minor fixes

--- js/about.js
+++ js/about.js
@@ -11,3 +11,12 @@ function initMap() {
 }
 initMap();
 
+
+function countChar(val) {
+    var len = val.value.length;
+    if (len >= 500) {
+      val.value = val.value.substring(0, 500);
+    } else {
+      $('#char-number').text(500 - len);
+    }
+  };
